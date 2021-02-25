aff00b3ad619bddde82b05d03be855a8e0472084
vexamd <65162246+vexamd@users.noreply.github.com>
2021-02-25 14:00:09 +0200

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,8 +1,17 @@
-sal = input('Enter Gross Salary: ')
-modulus = 24
-salary = sal
-imposit = salary * modulus / 100
-print('Your imposit is: ', imposit)
+name = input('What is your name? ')
+days = input('How many days did you work thin month? ')
+workedHours = input('How many hours did you work in a day?')
+salary = input('What is your salary per hour? ')
+workeddays = int(workedHours) * int(days)
+week = int(workedHours) * 5
+weektotal = int(week) * int(salary)
+totalsalary = int(workeddays) * int(salary)
+total = totalsalary * 24 / 100
+totalweek = weektotal * 24 / 100
+print(
+    'Hey ', name ,'!',
+    '\nYour salary for a week is: ', weektotal, 'With 24% taxes is: ', totalweek,
+    '\nYour salary for a month is: ', totalsalary, 'With 24% taxes is: ', total
+    )
 
-NetSalary = salary - imposit
-print("Your Net Salary is: ", NetSalary)
+
