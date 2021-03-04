1b747c1b2c04b6dcb28fc8eec5c2c0efa5c3de34
vexamd <65162246+vexamd@users.noreply.github.com>
2021-03-04 21:08:17 +0200

    salary

--- salary.py
+++ salary.py
@@ -1,17 +1,25 @@
-name = input('What is your name? ')
-days = input('How many days did you work thin month? ')
-workedHours = input('How many hours did you work in a day?')
-salary = input('What is your salary per hour? ')
-workeddays = int(workedHours) * int(days)
-week = int(workedHours) * 5
-weektotal = int(week) * int(salary)
-totalsalary = int(workeddays) * int(salary)
-total = totalsalary * 24 / 100
-totalweek = weektotal * 24 / 100
-print(
-    'Hey ', name ,'!',
-    '\nYour salary for a week is: ', weektotal, 'With 24% taxes is: ', totalweek,
-    '\nYour salary for a month is: ', totalsalary, 'With 24% taxes is: ', total
-    )
-
-
+class CalculatorNotNumberException(Exception):
+    print('Hey this is not a number')
+class salarycalculate:
+    name = input('What is your name? ')
+    days = input('How many days did you work thin month? ')
+    workedHours = input('How many hours did you work in a day?')
+    contract = input('What is your salary on contract? ')
+    salary = input('What is your salary per hour? ')
+    workeddays = int(workedHours) * int(days)
+    ç
+    workedsalary = workeddays * salary
+    if not isinstance(workeddays, int) or not isinstance(salary, int):
+        raise CalculatorNotNumberException()
+    totalgross = workedsalary + contract
+    if not isinstance(salary, int):
+        raise CalculatorNotNumberException()
+    netsalary = totalgross * 24 / 100
+    totalnet = totalgross - netsalary
+    print(
+        'Hey ', name ,'!',
+        '\nGross salary: ', totalgross, 'Lei'
+        '\nNet salary: ',  totalnet, 'Lei'
+        '\nContract salary: ', contract, 'Lei'
+        '\nWorked salary: ', workedsalary, 'Lei'
+        )
